9333390fd34a90da159a8f5eb247d5c99c9e28e4
Patrick Hultquist <patrick@monogram.io>
2022-03-09 11:47:48 -0500

    status

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 const Home = () => {
+	const status = 'Supported'
 	return (
 		<section className="container bg-[#141F2B] h-screen py-20 space-y-2 flex flex-col items-center text-white">
 			<h1 className="text-xl font-semibold">Monogram</h1>
