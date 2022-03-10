71755be2e6c0c710048a80e2ac619c63d71ccaee
Patrick Hultquist <patrick@monogram.io>
2022-03-09 20:12:25 -0500

    Clean up repository

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# [WebGL Stats](https://webgl.monogram.dev)
+# WebGL Stats
+
+View stats about WebGL for any device that connects to the internet.
+
+[View Stats &rarr;](https://webgl.monogram.dev)
--- pages/api/example.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-	res.status(200).json({ success: true, method: req.method })
-}
-
-export default handler
